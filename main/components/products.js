2383c3388307f3649ffb3e9ea8ed11291d29e63d
ryankrawz <krawczry@bc.edu>
2021-03-08 16:16:56 -0500

    Correct stylesheet reference

--- main/components/products.js
+++ main/components/products.js
@@ -7,7 +7,7 @@ export default function getProducts(products) {
                     <div id="${p.id}-container" class="product-container">
                         <img id="${p.id}-section-img" class="section-img" src="${p.longlogo}" alt="${p.name}">
                         <span id="${p.id}-shortdesc" class="product-shortdesc">${p.shortdesc}</span>
-                        <button id="${p.id}-button" class="product-button" style="color:white;background-color:${p.primarycolor};">
+                        <button id="${p.id}-button" class="product-button" style="background-color:${p.primarycolor};">
                             Learn More <i class="fas fa-angle-double-right"></i>
                         </button>
                     </div>
