d48b29eb4d71cb9e07df847d378b47e9ffae543c
Ronan Manvelian <ronanmanvelian@gmail.com>
2021-03-15 19:41:32 -0400

    changed color of navbar items

--- main/components/navBar.js
+++ main/components/navBar.js
@@ -2,13 +2,13 @@ export default function getNavbar(page){
     return `
     <nav>
     <div class="navbar">
-        <div class="nav-item"><a href="/recommender-systems-analysis">Home</a></div>
-        <div class="nav-item"><a href="/recommender-systems-analysis/?product=stitchfix">StitchFix</a></div>
-        <div class="nav-item"><a href="/recommender-systems-analysis/?product=spotify">Spotify</a></div>
-        <div class="nav-item"><a href="/recommender-systems-analysis/?product=netflix">Netflix</a></div>
-        <div class="nav-item"><a href="/recommender-systems-analysis/?product=instagram">Instagram</a></div>
+        <div class="nav-item"><a class="nav-link" href="/recommender-systems-analysis">Home</a></div>
+        <div class="nav-item"><a class="nav-link" href="/recommender-systems-analysis/?product=stitchfix">StitchFix</a></div>
+        <div class="nav-item"><a class="nav-link" href="/recommender-systems-analysis/?product=spotify">Spotify</a></div>
+        <div class="nav-item"><a class="nav-link" href="/recommender-systems-analysis/?product=netflix">Netflix</a></div>
+        <div class="nav-item"><a class="nav-link" href="/recommender-systems-analysis/?product=instagram">Instagram</a></div>
     </div>
-  </nav> 
-  
+  </nav>
+
     `;
   }
