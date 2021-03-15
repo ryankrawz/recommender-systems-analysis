38965fc787742910a8aab957e4d173040dbb9e05
fdznoriega <andresfdz7@gmail.com>
2021-03-15 12:45:10 -0400

    navbar band-aid

--- main/components/navBar.js
+++ main/components/navBar.js
@@ -2,11 +2,11 @@ export default function getNavbar(page){
     return `
     <nav>
     <div class="navbar">
-        <div class="nav-item"><a href="/">Home</a></div>
-        <div class="nav-item"><a href="/?product=stitchfix">StitchFix</a></div>
-        <div class="nav-item"><a href="/?product=spotify">Spotify</a></div>
-        <div class="nav-item"><a href="/?product=netflix">Netflix</a></div>
-        <div class="nav-item"><a href="/?product=instagram">Instagram</a></div>
+        <div class="nav-item"><a href="/recommender-systems-analysis">Home</a></div>
+        <div class="nav-item"><a href="/recommender-systems-analysis/?product=stitchfix">StitchFix</a></div>
+        <div class="nav-item"><a href="/recommender-systems-analysis/?product=spotify">Spotify</a></div>
+        <div class="nav-item"><a href="/recommender-systems-analysis/?product=netflix">Netflix</a></div>
+        <div class="nav-item"><a href="/recommender-systems-analysis/?product=instagram">Instagram</a></div>
     </div>
   </nav> 
   
