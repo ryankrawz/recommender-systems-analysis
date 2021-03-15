bca4a2a61ec73d20541dda668a6ba3ec62d747a4
fdznoriega <andresfdz7@gmail.com>
2021-03-15 15:29:14 -0400

    two column layout template done

--- product/components/section.js
+++ product/components/section.js
@@ -3,13 +3,13 @@
 function Section(section) {
     return(`
         <h2>${section.title}</h2>
-        <div class='.row'>
+        <div class='row'>
         
-            <div class='.col-6'>
+            <div class='col-6'>
                 <img src=${section.image} alt="${section.title}">
             </div>
             
-            <div class='.col-6'>
+            <div class='col-6'>
                 <p>${section.text}</p>
             </div>
    
